7255d19e4fa3a7b1aa19ac0a361317e818c8b300
Paula Catalina Curca <paul6465@edu.easj.dk>
2021-08-31 14:36:08 +0200

    Task2

---
+++ ClassRoom/ClassRoom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassRoom
+{
+    public class ClassRoom
+    {
+        public string ClassName { get; set; }
+        public List<Student> ClassList { get; set; }
+        public DateTime SemesterStart { get; set; }
+
+        public ClassRoom(string name, List<Student> list, DateTime Start)
+        {
+            ClassName = name;
+            ClassList = list;
+            SemesterStart = Start;
+        }
+    }
+}
