fd9521d05c797d95a62ee67b0fb017f45f6503c3
Paula Catalina Curca <paul6465@edu.easj.dk>
2021-08-31 14:49:49 +0200

    Task3,Task4

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -10,11 +10,18 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
-            ClassRoom Class=new ClassRoom("3Q",new List<Student>(), new DateTime(26-08-2019));
+            ClassRoom Class=new ClassRoom("3Q",new List<Student>(), new DateTime(2019,08,28));
             Class.ClassList.Add(new Student("Paula-Catalina Curca", 07, 17));
             Class.ClassList.Add(new Student("Radu Adumitroaei",10,03));
             Class.ClassList.Add(new Student("Alex Lazaroiu", 05, 23));
             Class.ClassList.Add(new Student("Kristaps Mezatucs", 11, 04));
+
+            foreach (var VARIABLE in Class.ClassList)
+            {
+                Console.WriteLine(VARIABLE.Name + VARIABLE.BirthDay+ VARIABLE.BirthMonth);
+            }
+
+            Console.ReadKey();
         }
     }
 }
