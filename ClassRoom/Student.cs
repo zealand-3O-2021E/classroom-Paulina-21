07059717dbc2ecb23391e255333cc70cfa6f2a2f
Paula Catalina Curca <paul6465@edu.easj.dk>
2021-08-31 14:33:08 +0200

    Task1

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassRoom
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int BirthMonth { get; set; }
+        public int BirthDay { get; set; }
+
+        public Student(string name, int month, int day)
+        {
+            Name = name;
+            BirthDay = day;
+            BirthMonth = month;
+        }
+    }
+}
