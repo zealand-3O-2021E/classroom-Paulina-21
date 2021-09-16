92eaa41ac48d796fbc0dbe76784e9741992a6f4b
Paula Catalina Curca <paul6465@edu.easj.dk>
2021-09-16 11:32:00 +0200

    SharedCalculator

---
+++ SharedCalculator/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SharedCalculator
+{
+    /// <summary>
+    /// This is the Calculator with its methods
+    /// </summary>
+    public class Calculator
+    {
+        /// <summary>
+        /// Adds two variables (a,b)
+        /// </summary>
+        /// <param name="a">first variable (double)</param>
+        /// <param name="b">second variable (double)</param>
+        /// <returns></returns>
+        public double Addition(double a, double b)
+        {
+            return a + b;
+        }
+        /// <summary>
+        /// Subtracts variable 'a' from 'b'
+        /// </summary>
+        /// <param name="a">first variable (double)</param>
+        /// <param name="b">second variable (double)</param>
+        /// <returns></returns>
+        public double Subtraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// Multiplies two variables (a,b)
+        /// </summary>
+        /// <param name="a">first variable (double)</param>
+        /// <param name="b">second variable (double)</param>
+        /// <returns></returns>
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// Divides variable b from a
+        /// </summary>
+        /// <param name="a">first variable (double)</param>
+        /// <param name="b">second variable (double)</param>
+        /// <returns></returns>
+        public double Division(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ SharedCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedCalculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [DataTestMethod]
+        [DataRow(-25,3.5)]
+        [DataRow(0,30)]
+        public void AdditionTest(double a, double b)
+        {
+            //Arrange
+            var cal = new Calculator();
+            var actual=cal.Addition(a, b);
+
+            //Act
+            double expected = a + b;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow(-25, 3.5)]
+        [DataRow(0, 30)]
+        public void SubtractionTest(double a, double b)
+        {
+            //Arrange
+            var cal = new Calculator();
+            var actual = cal.Subtraction(a, b);
+
+            //Act
+            double expected = a - b;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow(-25, 3.5)]
+        [DataRow(0, 30)]
+        public void MultiplyTest(double a, double b)
+        {
+            //Arrange
+            var cal = new Calculator();
+            var actual = cal.Multiply(a, b);
+
+            //Act
+            double expected = a * b;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow(-25, 3.5)]
+        [DataRow(0, 30)]
+        public void DivisionTest(double a, double b)
+        {
+            //Arrange
+            var cal = new Calculator();
+            var actual = cal.Division(a, b);
+
+            //Act
+            double expected = a/b;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
